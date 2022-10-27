50947b009ace5d2a38c7f968e7f8016e70ad2b21
Jp <jpsiegel8@gmail.com>
2022-10-27 12:53:17 -0400

    Fixed scroll text

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,5 +1,5 @@
-import React, { forwardRef } from 'react';
-import SocialLink from './SocialLink';
+import React, { forwardRef } from "react";
+import SocialLink from "./SocialLink";
 
 const Overlay = forwardRef(({ caption, scroll }, ref) => (
   <div
@@ -7,38 +7,38 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
     onScroll={(e) => {
       scroll.current =
         e.target.scrollTop / (e.target.scrollHeight - window.innerHeight);
-      caption.current.innerText = scroll.current === 1 ? '' : 'Scroll down';
+      caption.current.innerText = scroll.current < 0.05 ? "Scroll down" : "";
     }}
     className="scroll"
   >
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <div className="blob">
         <h1>John Siegel</h1>
         Software Engineer with an interest in Machine learning, Graphics, and
         Web Development.
         <br />
-        <div style={{ textAlign: 'left', paddingTop: '32px' }}>
+        <div style={{ textAlign: "left", paddingTop: "32px" }}>
           <a href="/Resume.pdf" target="_blank" rel="noreferrer">
-            <button className="button" style={{ border: 'none' }}>
+            <button className="button" style={{ border: "none" }}>
               <span className="button-text">Download CV</span>
             </button>
           </a>
         </div>
       </div>
     </div>
-    <div style={{ height: '225vh' }}>
+    <div style={{ height: "225vh" }}>
       <div className="blob">
         <h1>About</h1>
         <img
           src="portrait.jpg"
           alt="John Siegel"
           style={{
-            borderRadius: '50%',
-            width: '45%',
-            height: '45%',
-            float: 'left',
-            margin: '40px 20px 30px 0',
-            shapeOutside: 'circle(50%)',
+            borderRadius: "50%",
+            width: "45%",
+            height: "45%",
+            float: "left",
+            margin: "40px 20px 30px 0",
+            shapeOutside: "circle(50%)",
           }}
         />
         John Siegel is a software developer expecting to graduate with a B.S.
@@ -58,18 +58,18 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         spends most of his time pursuing his studies in those fields, he also
         enjoys his hobbies as well. John runs frequently, listens to podcasts
         such as Lex Fridman, and reads science-fiction and manga.
-        <p style={{ lineHeight: '1rem', fontStyle: 'italic' }}>
+        <p style={{ lineHeight: "1rem", fontStyle: "italic" }}>
           "You can't learn everything, but you have to convince yourself that
           you can learn anything..."
         </p>
-        <p style={{ textAlign: 'right' }}>- John Carmack</p>
+        <p style={{ textAlign: "right" }}>- John Carmack</p>
       </div>
     </div>
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <div className="blob">
         <h1>Skills</h1>
         <table>
-          <tr style={{ textAlign: 'left' }}>
+          <tr style={{ textAlign: "left" }}>
             <th>Languages</th>
           </tr>
           <tr>
@@ -84,7 +84,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
             <td>Java</td>
             <td>Verilog</td>
           </tr>
-          <tr style={{ padding: '8px', textAlign: 'left' }}>
+          <tr style={{ padding: "8px", textAlign: "left" }}>
             <th>Software and Tools</th>
           </tr>
           <tr>
@@ -122,10 +122,10 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         </table>
       </div>
     </div>
-    <div style={{ height: '250vh' }}>
+    <div style={{ height: "250vh" }}>
       <div className="blob">
         <h1>Experience</h1>
-        <h3 style={{ textDecorationLine: 'underline' }}>05/22 - Current</h3>
+        <h3 style={{ textDecorationLine: "underline" }}>05/22 - Current</h3>
         <h4>Stellar Science, Jr. Scientific Software Engineer</h4>
         <ul>
           <li>
@@ -138,7 +138,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
             Practicing test-driven development techniques in a large codebase
           </li>
         </ul>
-        <h3 style={{ textDecorationLine: 'underline' }}>03/22 - Current</h3>
+        <h3 style={{ textDecorationLine: "underline" }}>03/22 - Current</h3>
         <h4>Phitnest, Development Team Lead</h4>
         <ul>
           <li>Leading a team of 8 developers</li>
@@ -152,7 +152,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
           </li>
           <li>Working directly with UX professionals and market researchers</li>
         </ul>
-        <h3 style={{ textDecorationLine: 'underline' }}>10/21 - Current</h3>
+        <h3 style={{ textDecorationLine: "underline" }}>10/21 - Current</h3>
         <h4>Project Shinobi, C++ Developer</h4>
         <ul>
           <li>Developing a video game with Unreal Engine 5</li>
@@ -168,7 +168,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         </ul>
       </div>
     </div>
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <div className="blob">
         <h1>Education</h1>
         <img
@@ -179,10 +179,10 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         <h3>B.S. Computer Science, Virginia Tech, 2023</h3>
       </div>
     </div>
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <div className="blob">
         <h1>Links</h1>
-        <div style={{ lineHeight: '64px', width: '256px' }}>
+        <div style={{ lineHeight: "64px", width: "256px" }}>
           <SocialLink url="https://github.com/JohnSiegel" text="GitHub" />
           <SocialLink
             url="https://www.linkedin.com/in/john-siegel-560333166/"
