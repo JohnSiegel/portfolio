4ab4d40f306e0b01d492984566a8764c1691c436
John Siegel <jpsiegel8@gmail.com>
2024-06-04 19:33:09 -0400

    Updated information

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -14,8 +14,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
     <div style={{ height: "100vh" }}>
       <div className="blob">
         <h1>John Siegel</h1>
-        Software Engineer with an interest in Machine learning, Graphics, and
-        Web Development.
+        Software Engineer with a passion for high quality code.
         <br />
         <div style={{ textAlign: "left", paddingTop: "32px" }}>
           <a href="/Resume.pdf" target="_blank" rel="noreferrer">
@@ -26,8 +25,8 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         </div>
       </div>
     </div>
-    <div style={{ height: "100vh" }}>
-      <div className="blob">
+    <div style={{ height: "120vh" }}>
+      <div className="blob" style={{maxWidth: "600px"}}>
         <h1>About</h1>
         <img
           src="portrait.jpg"
@@ -41,12 +40,9 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
             shapeOutside: "circle(50%)",
           }}
         />
-        Hi, I'm John, a software developer expecting to graduate with a B.S.
-        Computer Science from Virginia Tech in Fall 2023. I am particularly
-        interested in Machine Learning, Graphics, and Web Development.
+        Hi, I'm John. I have worked as a software developer for Stellar Science since May 2022. I graduated from Virginia Tech with a B.S. Computer Science in January 2023. I have experience writing C++, Python, Java, Typescript, and Dart in production applications with varying amounts of active users. I am most experienced in laser and satellite simulations, the Monte Carlo method, GUIs, distributed computing, websites, and mobile apps. I have worked in teams ranging in sizes from 3-30 developers. I have experience interacting with customers, attending conferences, and putting AGILE methodologies into practice.
         <p style={{ lineHeight: "1rem", fontStyle: "italic" }}>
-          "You can't learn everything, but you have to convince yourself that
-          you can learn anything..."
+          "You can't learn everything, but you have to convince yourself that you can learn anything..."
         </p>
         <p style={{ textAlign: "right" }}>- John Carmack</p>
       </div>
@@ -59,7 +55,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
             <th>Languages</th>
           </tr>
           <tr>
-            <td>C, C++ & CMake</td>
+            <td>C++ & CMake</td>
             <td>Dart</td>
           </tr>
           <tr>
@@ -111,48 +107,45 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
     <div style={{ height: "250vh" }}>
       <div className="blob">
         <h1>Experience</h1>
-        <h3 style={{ textDecorationLine: "underline" }}>05/22 - Current</h3>
-        <h4>Stellar Science, Scientific Software Engineer Intern</h4>
+        <h3 style={{ textDecorationLine: "underline" }}>01/24 - Current</h3>
+        <h4>Sr. Software Developer, Stellar Science</h4>
         <ul>
           <li>
-            Developing C++ software for physics simulation and image
-            manipulation
+            Personally appointed as the first ever "Code-Quality Enforcer" by the CEO - Responsible for reviewing the entire company's codebase to find refactoring and improvement opportunities
           </li>
-          <li>Working on tasks related to computer vision and graphics</li>
-          <li>Creating GUIs with Qt</li>
           <li>
-            Practicing test-driven development techniques in a large codebase
+            Leading the software development effort for multiple projects of varying team size in collaboration with Air Force Research Lab, Raytheon, and other defense contractors
           </li>
+          <li>Meeting with customers, attending conferences, and writing user stories</li>
         </ul>
-        <h3 style={{ textDecorationLine: "underline" }}>03/22 - Current</h3>
-        <h4>Phitnest, Development Team Lead</h4>
+        <h3 style={{ textDecorationLine: "underline" }}>05/22 - 01/24</h3>
+        <h4>Software Developer, Stellar Science</h4>
         <ul>
+          <li>Practicing test-driven development and creating user story centric tests</li>
           <li>
-            Leading a team of 8 developers from India, New Zealand, Nigeria, and
-            the United States
-          </li>
-          <li>Writing technical documentation and a developer wiki</li>
-          <li>
-            Organizing sprints, holding scrum, and using a Kanban on Notion
+            Developing physics simulations for high performance computing systems
           </li>
           <li>
-            Gaining experience using MongoDB, Express, Nodejs, and Flutter to
-            develop a cross-platform mobile app
+            Working in teams ranging in size from 5-30 people - including analysts, technical writers, and other software developers
           </li>
           <li>Working directly with UX professionals and market researchers</li>
         </ul>
-        <h3 style={{ textDecorationLine: "underline" }}>10/21 - Current</h3>
-        <h4>Project Shinobi, C++ Developer</h4>
+        <h3 style={{ textDecorationLine: "underline" }}>03/22 - 03/24</h3>
+        <h4>Chief Technology Officer, Umee</h4>
         <ul>
-          <li>Developing a video game with Unreal Engine 5</li>
-          <li>Working in a team of 20 developers</li>
+          <li>Led a team of eight developers to create an MVP with Flutter and AWS products</li>
+          <li>Worked alongside a design and research team to craft user stories</li>
           <li>
-            Working with complex shaders, 3D models, texturing, dynamic
-            lighting, animation, and UI
+            Collaborated with marketing and business development teams to ensure alignment between our technological and business goals
           </li>
+        </ul>
+        <h3 style={{ textDecorationLine: "underline" }}>01/21 - 03/22</h3>
+        <h4>Lead Developer, Project Shinobi</h4>
+        <ul>
+          <li>Designed the entire system architecture for a 3D MMO game using AWS and Unreal Engine 4</li>
+          <li>Led a team of 10 developers to implement the system architecture specification under an AGILE framework</li>
           <li>
-            Writing gameplay algorithms in C++ using a multitude of large
-            libraries
+            Released the game and earned 4.5 million+ views on a YouTube demo <a href="https://www.youtube.com/watch?v=U30y2SjIK6o">(link)</a>
           </li>
         </ul>
       </div>
@@ -165,7 +158,7 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
           alt="Virginia Tech Logo"
           style={{ width: 150, height: 150 }}
         />
-        <h3>B.S. Computer Science, Virginia Tech, 2023</h3>
+        <h3>B.S. Computer Science, Virginia Tech, 2024</h3>
       </div>
     </div>
     <div style={{ height: "100vh" }}>
@@ -177,14 +170,10 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
             url="https://www.linkedin.com/in/john-siegel-560333166/"
             text="LinkedIn"
           />
-          <SocialLink url="mailto:jp@john-siegel.com" text="E-Mail" />
+          <SocialLink url="mailto:jpsiegel8@gmail.com" text="E-Mail" />
           <SocialLink
             url="https://stackoverflow.com/users/17799086/john-siegel"
             text="Stackoverflow"
-          />
-          <SocialLink
-            url="https://discordapp.com/users/338154696546058250/"
-            text="Discord"
           />
         </div>
       </div>
